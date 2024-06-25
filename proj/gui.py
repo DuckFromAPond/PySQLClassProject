880734b051b202411186671c0ab51e2d082c4592
Reaksmey <64191668+DuckFromAPond@users.noreply.github.com>
2024-06-25 15:37:23 +0700

    gui.py

--- proj/gui.py
+++ proj/gui.py
@@ -2026,36 +2026,36 @@ create table users
 	ID int Primary key auto_increment,
 	email varchar(30),
 	username varchar(20) NOT NULL unique,
-	password varchar(20)
+	password varchar(20) NOT NULL
 );
 
 create table hall1
 (
 	SeatID int primary key,
-    ID int,
-    voucher boolean
+   	ID int,
+   	voucher boolean
 );
 
 
 create table hall2  
 (
 	SeatID int primary key,
-    ID int,
-    voucher boolean
+    	ID int,
+    	voucher boolean
 );
 
 create table hall3
 (
 	SeatID int primary key,
-    ID int,
-    voucher boolean
+    	ID int,
+    	voucher boolean
 );
 
 create table hall4
 (
 	SeatID int primary key,
-    ID int,
-    voucher boolean
+    	ID int,
+    	voucher boolean
 );
 
 !!!!! DON'T FORGET TO ADD YOUR OWN PASS TO THE CONNECTION !!!!
